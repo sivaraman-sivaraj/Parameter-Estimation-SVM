04fcbb6fd2fab0f6a987d95b63073623aebd429d
sramarjunan <63408869+sramarjunan@users.noreply.github.com>
2020-06-25 10:00:36 +0530

    Update regression_coefficient.py

--- code/regression_coefficient.py
+++ code/regression_coefficient.py
@@ -11,79 +11,84 @@ print(__doc__)
 import hydrodynamics as hdy
 
 """
-TV, RAC, surgeXC, yawRC, swayYC, nU, nV,nR # importing variables from hydrodynamics
+surgeXC, yawRC, swayYC, nU, nV,nR # importing variables from hydrodynamics
 
-x,X,y,Y,z,Z,pi,pi1,theta,theta1,shi,\
-shi1,epn1, epn2, epn3,epn4, epn5, epn6,u,v,w,p,q,r,rudderAngle, rpm
+u,v,r,U,rac - input data also importable
 """
 
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-surgeXC_train = hdy.surgeXC
-swayYC_train = hdy.swayYC
-yawRC_train = hdy.yawRC
+surgeXC = hdy.surgeXC
+swayYC = hdy.swayYC
+yawRC = hdy.yawRC
 
-_nU_train = hdy.nU
-_nV_train = hdy.nV
-_nR_train = hdy.nR
+nU = hdy.nU
+nV = hdy.nV
+nR = hdy.nR
 
-
-
-# surgeXC_train, surgeXC_test, nU_train, nU_test = train_test_split(hdy.surgeXC,hdy.nU,random_state = 0)
-# swayYC_train, swayYC_test, nV_train, nV_test = train_test_split(hdy.swayYC,hdy.nV, random_state = 0)     
-# yawRC_train, yawRC_test, nR_train, nR_test = train_test_split(hdy.yawRC,hdy.nR, random_state = 0)   
-
-def ramp_signal(ip,Lambda):
-    op = []
-    for i in range(len(ip)):
-        temp = ip[i]+Lambda
-        op.append(temp)
-    return op
-        
-nU_train = ramp_signal(_nU_train, 0.01)
-nV_train = ramp_signal(_nV_train, 0.01)
-nR_train = ramp_signal(_nR_train, 0.01)
     
-# nU_test = ramp_signal(_nU_test, 0.01)
-# nV_test = ramp_signal(_nV_test, 0.01)
-# nR_test = ramp_signal(_nR_test, 0.01)
+nU_ramp = hdy.nU_ramp
+nV_ramp = hdy.nV_ramp
+nR_ramp = hdy.nR_ramp
 
 
 from sklearn.svm import LinearSVR
 
-linear_svm = LinearSVR(C=1e08,fit_intercept = False, dual = True ,
-                       epsilon = 1e-6, loss = 'squared_epsilon_insensitive',
-                       max_iter = 10000, random_state = None, tol = 0.000001,
-                       verbose = 0).fit(surgeXC_train, nU_train)
+linear_svm = LinearSVR(C=1e08,fit_intercept = True, dual = True ,
+                        epsilon = 1e-6, loss = 'squared_epsilon_insensitive',
+                        max_iter = 10000, random_state = None, tol = 0.000001,
+                        verbose = 0).fit(surgeXC, nU)
 coefLinear = linear_svm.coef_
-print("Train set accuracy of Surge on LinearSVR method: {:.2f}".format(linear_svm.score(surgeXC_train,nU_train)))
+
+linear_svm_ramp = LinearSVR(C=1,fit_intercept = True, dual = True ,
+                        epsilon = 1e-6, loss = 'squared_epsilon_insensitive',
+                        max_iter = 10000, random_state = None, verbose = 0).fit(surgeXC, nU_ramp)
+coefLinear_ramp = linear_svm_ramp.coef_
+print("Train set accuracy of Surge on LinearSVR method: {:.2f}".format(linear_svm.score(surgeXC,nU)))
 
 
 
-linear_svm1 = LinearSVR(C=1e08,fit_intercept = False, dual = True ,
-                       epsilon = 1e-6, loss = 'squared_epsilon_insensitive',
-                       max_iter = 10000, random_state = None, tol = 0.000001,
-                       verbose = 0).fit(swayYC_train,nV_train)
+linear_svm1 = LinearSVR(C=1e08,fit_intercept = True, dual = True ,
+                        epsilon = 1e-6, loss = 'squared_epsilon_insensitive',
+                        max_iter = 10000, random_state = None, tol = 0.000001,
+                        verbose = 0).fit(swayYC,nV)
 
 coefLinear1 = linear_svm1.coef_
-print("Train set accuracy of Sway on LinearSVR method: {:.2f}".format(linear_svm1.score(swayYC_train,nV_train)))
+linear_svm1_ramp = LinearSVR(C=1e08,fit_intercept = True, dual = True ,
+                        epsilon = 1e-6, loss = 'squared_epsilon_insensitive',
+                        max_iter = 10000, random_state = None, tol = 0.000001,
+                        verbose = 0).fit(swayYC,nV_ramp)
+
+coefLinear1_ramp = linear_svm1_ramp.coef_
+
+print("Train set accuracy of Sway on LinearSVR method: {:.2f}".format(linear_svm1.score(swayYC,nV)))
 
 
 
-linear_svm2 = LinearSVR(C=1e08,fit_intercept = False, dual = True ,
-                       epsilon = 1e-6, loss = 'squared_epsilon_insensitive',
-                       max_iter = 10000, random_state = None, tol = 0.000001,
-                       verbose = 0).fit(yawRC_train,nR_train)
+linear_svm2 = LinearSVR(C=1e08,fit_intercept = True, dual = True ,
+                        epsilon = 1e-6, loss = 'squared_epsilon_insensitive',
+                        max_iter = 10000, random_state = None, tol = 0.000001,
+                        verbose = 0).fit(yawRC,nR)
 
 coefLinear2 = linear_svm2.coef_
-print("Train set accuracy of Yaw on LinearSVR method: {:.2f}".format(linear_svm2.score(yawRC_train,nR_train)))
 
+linear_svm2_ramp = LinearSVR(C=1e08,fit_intercept = True, dual = True ,
+                        epsilon = 1e-6, loss = 'squared_epsilon_insensitive',
+                        max_iter = 10000, random_state = None, tol = 0.000001,
+                        verbose = 0).fit(yawRC,nR_ramp)
+
+coefLinear2_ramp = linear_svm2_ramp.coef_
+print("Train set accuracy of Yaw on LinearSVR method: {:.2f}".format(linear_svm2.score(yawRC,nR)))
 
 
 np.save('surge_regression_coeff.npy', coefLinear)
 np.save('sway_regression_coeff.npy', coefLinear1)
 np.save('yaw_regression_coeff.npy', coefLinear2)
+
+np.save('surge_regression_coeff_ramp.npy', coefLinear_ramp)
+np.save('sway_regression_coeff_ramp.npy', coefLinear1_ramp)
+np.save('yaw_regression_coeff_ramp.npy', coefLinear2_ramp)
 
 
 
